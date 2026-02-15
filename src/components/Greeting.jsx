5d8723de9c5141d1b3d426e9d20e164b0e9d11e2
CuLoo <140522287+Viruatios@users.noreply.github.com>
2026-02-15 21:15:50 +0800

    被注水的组件Astro Islands

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,31 @@
+/*与教程不同，我将采用React而非Preact */
+
+import { useState } from "react";
+
+export default function Greeting({messages}){
+    const getRandomMessage = () =>
+        messages[Math.floor(Math.random() * messages.length)];
+
+    const getNextMessage = (current) => {
+        if (messages.length <= 1) {
+            return current ?? "";
+        }
+
+        let next = current;
+        while (next === current) {
+            next = getRandomMessage();
+        }
+        return next;
+    };
+    
+    const [greeting, setGreeting] = useState(getNextMessage());
+
+    return (
+        <div>
+            <h3>{greeting}!</h3>
+            <button onClick = {() => setGreeting(getNextMessage(greeting))}>
+                换一句
+            </button>
+        </div>
+    )
+}
